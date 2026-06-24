ea6d13dc317b41b26685af7b4872a711d439d14f
Showmik <mutasim.showmik@efficientsoftwaresolutions.com>
2026-06-24 12:16:04 +0600

    initial

---
+++ LibraryManagementSystem/Models/Entities/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagementSystem.Models.Entities
+{
+    //1 Author can have many books (1 --> many)
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Bio {  get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>(); 
+    }
+}
---
+++ LibraryManagementSystem/Models/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryManagementSystem.Models.Entities
+{
+    //1 book --> 1 Author and 1 book ---> 1 Category
+    public class Book
+    {
+        public required Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string ISBN { get; set; }
+        public required int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category {  get; set; }
+        public required int TotalCopies { get; set; }
+        public required int AvailableCopies { get; set; }
+    }
+}
